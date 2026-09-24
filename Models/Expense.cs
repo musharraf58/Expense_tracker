b20cc2af346dc087f75e5c1bea1ab02b203aa39f
Musharraf Amadbekov <amadbekovmusharraf675@gmail.com>
2026-09-24 11:36:56 +0600

    Move expense endpoints to controller

---
+++ Models/Expense.cs
@@ -0,0 +1,9 @@
+namespace ExpenseTracker.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public string Description { get; set; } = "";
+    public DateTime Date { get; set; }
+}
